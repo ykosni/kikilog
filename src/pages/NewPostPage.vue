48be860f1f8a39f13073b9e33b4f4ba4c66aad2a
yuki.osanai <ykosni.ykosni@gmail.com>
2025-04-28 05:29:32 +0000

    新規投稿：URL入力後自動で楽曲情報を取得

--- src/pages/NewPostPage.vue
+++ src/pages/NewPostPage.vue
@@ -7,8 +7,10 @@
         <div>
           <label class="text-gray-100 text-lg font-bold mb-4 flex items-center gap-2">
             <Link2 class="w-6 h-6 text-[#1ed760] shrink-0" :stroke-width="3"/>
-            Spotify楽曲URL</label>
+            Spotify楽曲URL
+          </label>
           <input v-model="spotifyUrl" type="text" placeholder="https://open.spotify.com/〜"
+            @input="handleSpotifyRequest"
             class="w-full bg-[#2a2a2a] border border-[#4a4a4a] rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#1ed760]" />
         </div>
 
